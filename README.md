20cc56f486560f9c7df51374bddbdc1931732c24
pl-rafal-jaworski <rafak97@gmail.com>
2019-05-26 14:00:32 +0200

    Release

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# indeksownik
+# Indeksownik
 Odkoduj indeksy
+
+##### Podaj indeksy oraz bazę plików z indeksami, a program automatycznie utworzy plik, który odkoduje te indeksy czyli pokaże imię i nazwisko osoby pod tym indeksem oraz pokaże na jakim kierunku studiuje.
+***
+
+
+### Format pliku z bazą danych
+Jest to plik o rozszerzeniu .csv
+
+Dane muszą być w formacie:
+
+**Pierwszy wiersz powinien zawierać nazwy kolumn:**
+
+Pełny indeks,Indeks,Imię,Imię 2,Nazwisko,Kierunek 1,Specjalność 1,Kierunek 2,Specjalność 2
+
+### Format pliku z indeksami do wyszukania
+Są to po prostu numery indeksów w nowych linijkach zapisane do zwykłego pliku tekstowego
+***
+
+### Działanie programu
+**Wymagany jest Python3 **
+
+Aby uruchomić program należy pobrać zawartośc repozytorium a następnie będąc w katalogu tego repozytorium należy wpisać komendę:
+
+*python3 script.py baza.csv indeksy.txt*
+
+Wygeneruje to plik "wynik.csv" który zawiera szukane przez nas indeksy wraz z danymi
---
+++ script.py
@@ -0,0 +1,93 @@
+import csv
+import sys
+
+def buildDatabase(databaseFileName):
+    database = []
+    with open(databaseFileName) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        line = 0
+        for row in csv_reader:
+            if line == 0:
+                line += 1
+            else:
+                database.append(row)
+        return database
+
+def buildCheckFile(checkFileName):
+    list = []
+    with open (checkFileName) as file:
+        for row in file:
+            list.append(row.rstrip())
+        return list
+
+def startup():
+    if len(sys.argv) !=3 :
+        print("Błąd!\nPrawidłowa składnia to: python3 script.py baza plik_z_indeksami")
+        #return False
+    else:
+        global databaseFileName
+        global checkFileName
+        databaseFileName = sys.argv[1]
+        checkFileName = sys.argv[2]
+        #return True
+        mainCont()
+def mainCont():
+    global database
+    global list
+    database = buildDatabase(databaseFileName)
+    list = buildCheckFile(checkFileName)
+    #print (list)
+    findInDatabase()
+    saveAsCSV()
+
+def findInDatabase():
+    global referenceTable
+    referenceTable = []
+    global savedIndexes
+    savedIndexes = []
+    for index in list:
+        for row in database:
+            if index == row[1]:
+                person = ""
+                person += row[1] + ";"
+                person += row[2] + ";"
+                person += row[3] + ";"
+                person += row[4] + ";"
+                person += row[5] + ";"
+                person += row[6] + ";"
+                person += row[7] + ";"
+                person += row[8]
+                referenceTable.append(person)
+                savedIndexes.append(index)
+                #print(referenceTable)
+    #print(database[0][1])
+def notFoundIndexes():
+    global notFound
+    notFound = []
+    for person in list:
+        if person not in savedIndexes:
+            notFound.append(person)
+    if len(notFound) > 0:
+        return True
+    else:
+        return False
+
+
+def saveAsCSV():
+    with open("wynik.csv", 'w') as file:
+        file.write('Indeks;Imię;Imię 2;Nazwisko;Kierunek 1;Specjalność 1;Kierunek 2;Specjalność 2\n')
+        for person in referenceTable:
+            file.write(person)
+            file.write('\n')
+        if notFoundIndexes() :
+            file.write("\n\nBrak danych o indeksach\n")
+            for p in notFound:
+                file.write(p)
+
+
+
+
+
+
+
+startup()
